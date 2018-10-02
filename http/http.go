5920a9e9eecf1d45ac91481cad5379c2214f0f93
张添 <zhangtian@mail.hdzuoye.com>
2018-10-02 15:55:54 +0800

    Dev: 添加http相关工具

--- http/http.go
+++ http/http.go
@@ -1,6 +1,10 @@
 package http
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func AllowAllCrossOrigin(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -10,4 +14,9 @@ func AllowAllHeaders(w http.ResponseWriter, r *http.Request) {
 	for _, h := range r.Header["Access-Control-Request-Headers"] {
 		w.Header().Set("Access-Control-Allow-Headers", h)
 	}
+}
+
+var DefaultOptionsHandler = func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	AllowAllCrossOrigin(w)
+	AllowAllHeaders(w, r)
 }
