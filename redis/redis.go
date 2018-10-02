f2936ccea95368954a633ac08d6d8edb6469eed2
张添 <zhangtian@mail.hdzuoye.com>
2018-10-02 17:18:42 +0800

    Feat: 添加redis相关工具

---
+++ redis/redis.go
@@ -0,0 +1,19 @@
+package redis
+
+import (
+	"github.com/go-redis/redis"
+	"istudybookgitlab.hdzuoye.com/istudybook/server/golang-util.git"
+)
+
+func LoadEnvConfig(config *redis.Options) {
+	if config == nil {
+		config = &redis.Options{}
+	}
+	config.Addr = util.GetEnvStr("REDIS_ADDR", util.StrFallback(config.Addr, "127.0.0.1:6379"))
+	config.Password = util.GetEnvStr("REDIS_PASSWORD", util.StrFallback(config.Password, ""))
+	config.DB = util.GetEnvInt("REDIS_DB", config.DB)
+}
+
+func OpenRedis(config *redis.Options) *redis.Client {
+	return redis.NewClient(config)
+}
