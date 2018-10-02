f96de7649cb331dbce9fcf18ce274365510514c1
张添 <zhangtian@mail.hdzuoye.com>
2018-10-03 02:02:43 +0800

    Dev: 添加graphql相关工具

---
+++ graphqlutil/pagination.go
@@ -0,0 +1,35 @@
+package graphqlutil
+
+import "istudybookgitlab.hdzuoye.com/istudybook/server/golang-util.git"
+
+func ParsePaginationFirst(args map[string]interface{}, def, max uint64) (first uint64) {
+	first = args["first"].(uint64)
+	if first == 0 {
+		first = def
+	} else if first > max {
+		first = max
+	}
+	return
+}
+
+func ParsePaginationUint64After(args map[string]interface{}) (after uint64, err error) {
+	base64After := args["after"].(string)
+	if base64After != "" {
+		after, err = util.Base64ToUint64(base64After)
+		if err != nil {
+			return
+		}
+	} else {
+		after = 0
+	}
+	return
+}
+
+func ParsePaginationInt64After(args map[string]interface{}) (after int64, err error) {
+	uint64After, err := ParsePaginationUint64After(args)
+	if err != nil {
+		return
+	}
+	after = int64(uint64After)
+	return
+}
