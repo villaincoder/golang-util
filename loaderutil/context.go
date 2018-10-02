5808601e61303828cda56ea292d7738b42cb117c
张添 <zhangtian@mail.hdzuoye.com>
2018-10-03 00:11:34 +0800

    Dev: 添加loader相关工具

--- loaderutil/context.go
+++ loaderutil/context.go
@@ -11,7 +11,7 @@ func (server *Server) NewContext(ctx context.Context) context.Context {
 	return context.WithValue(ctx, CtxKey, server)
 }
 
-func GetLoaderServer(ctx context.Context) (handler *RequestHandler, err error) {
+func GetRequestHandler(ctx context.Context) (handler *RequestHandler, err error) {
 	handler = ctx.Value(CtxKey).(*RequestHandler)
 	if handler == nil {
 		err = errors.New("context not found loader request handler")
